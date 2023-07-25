1066a7dd8e1fd1c0e996bd08ee3404da27a2100f
pauljoohyunkim <pauljoohyunkim@gmail.com>
2023-07-25 12:51:35 +0200

    Positional

--- src/octocurl2.cpp
+++ src/octocurl2.cpp
@@ -14,18 +14,21 @@ int main(int argc, char** argv)
      * --help, -h: Help Message
      * --sort, -s: Sort Queue
     */
-    po::options_description desc("Allowed options");
-    desc.add_options()
+    po::options_description generic("Generic options");
+    generic.add_options()
         ("help,h", "produce help message")
         ("sort,s", "sort queue")
         ("urls", po::value<std::vector<std::string>>(), "url")
     ;
+    po::positional_options_description p;
+    p.add("urls", -1);
     po::variables_map vm;
-    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::store(po::command_line_parser(argc, argv).options(generic).positional(p).run(), vm);
     po::notify(vm);
+    // For each option, do its action.
     if (vm.count("help"))
     {
-        std::cout << desc << "\n";
+        std::cout << generic << "\n";
         return 1;
     }
     if (vm.count("sort"))
@@ -33,7 +36,11 @@ int main(int argc, char** argv)
         octocurlOptions.sort = true;
         std::cout << "Sorting enabled.";
     }
-    std::cout << vm["urls"].as<std::vector<std::string>>()[0];
+    auto urls = vm["urls"].as<std::vector<std::string>>();
+    for (auto url : urls)
+    {
+        std::cout << url << std::endl;
+    }
 
     return EXIT_SUCCESS;
 }
