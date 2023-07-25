cd54ca6700c02c0c1388259016c7c4e1331ffb42
pauljoohyunkim <pauljoohyunkim@gmail.com>
2023-07-25 11:35:30 +0200

    Basic command arguments parsing, basic download_task, and boost dependency

---
+++ src/download_task.hpp
@@ -0,0 +1,16 @@
+#ifndef DOWNLOAD_TASK_HPP
+#define DOWNLOAD_TASK_HPP
+
+#include <string>
+
+struct DownloadTask
+{
+    DownloadTask(std::string url, std::string name);
+    std::string file_url {};
+    std::string file_name {};
+    unsigned int file_size { 0 };
+    unsigned int file_downloaded_length { 0 };
+    bool downloaded { false };
+};
+
+#endif  // DOWNLOAD_TASK_HPP
---
+++ src/octocurl2.cpp
@@ -0,0 +1,36 @@
+#include <boost/program_options.hpp>
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include "download_task.hpp"
+
+namespace po = boost::program_options;
+
+int main(int argc, char** argv)
+{
+    /*
+     * Parsing command line options
+     * --help, -h: Help Message
+     * --sort, -s: Sort Queue
+    */
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help,h", "produce help message")
+        ("test,t", "test message")
+    ;
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    if (vm.count("help"))
+    {
+        std::cout << desc << "\n";
+        return 1;
+    }
+    if (vm.count("sort"))
+    {
+        std::cout << "Sorting enabled.";
+    }
+
+    return EXIT_SUCCESS;
+}
