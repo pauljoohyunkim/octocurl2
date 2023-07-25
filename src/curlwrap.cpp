00ac199947bbaa580072f8776c08787ba5cabbf5
pauljoohyunkim <pauljoohyunkim@gmail.com>
2023-07-25 19:46:00 +0200

    Curl initialization error checking

--- src/curlwrap.cpp
+++ src/curlwrap.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
+#include <stdexcept>
 #include "curlwrap.hpp"
 
 std::unique_ptr<CURL, void(*)(CURL*)> smart_curl_constructor()
 {
     CURL* curl { curl_easy_init() };
-    auto smart_curl_pointer = std::unique_ptr<CURL, void(*)(CURL*)>(curl, smart_curl_deleter);
-    return smart_curl_pointer;
+
+    if (curl)
+    {
+        auto smart_curl_pointer = std::unique_ptr<CURL, void (*)(CURL *)>(curl, smart_curl_deleter);
+        return smart_curl_pointer;
+    }
+    else
+    {
+        throw std::runtime_error("CURL failed to initialize!");
+    }
 }
 
 void smart_curl_deleter(CURL* curl)
