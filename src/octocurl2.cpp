f5f17699fe3a810a7bcae30a310f6aa2f415312c
pauljoohyunkim <pauljoohyunkim@gmail.com>
2023-07-25 19:15:19 +0200

    Output name detection

--- src/octocurl2.cpp
+++ src/octocurl2.cpp
@@ -2,11 +2,15 @@
 #include <iostream>
 #include <cstdlib>
 #include <vector>
+#include <string>
+#include <regex>
 #include "download_task.hpp"
+#include "filename.hpp"
 
 namespace po = boost::program_options;
 
 static void parseCommandLineArguments(int argc, char** argv, OctocurlTaskManager& tm);
+static void detectOutputName(std::string string, std::string& url, std::string& filename, std::string delimiter="->");
 
 int main(int argc, char** argv)
 {
@@ -30,13 +34,13 @@ static void parseCommandLineArguments(int argc, char** argv, OctocurlTaskManager
     ;
     po::options_description hidden;
     hidden.add_options()
-        ("urls", po::value<std::vector<std::string>>(), "url")
+        ("inputs", po::value<std::vector<std::string>>(), "inputs")
     ;
 
     po::options_description composite;
     composite.add(generic).add(hidden);
     po::positional_options_description p;
-    p.add("urls", -1);
+    p.add("inputs", -1);
     po::variables_map vm;
     po::store(po::command_line_parser(argc, argv).options(composite).positional(p).run(), vm);
     po::notify(vm);
@@ -51,13 +55,33 @@ static void parseCommandLineArguments(int argc, char** argv, OctocurlTaskManager
         tm.options.sort = true;
         std::cout << "Sorting enabled." << std::endl;
     }
-    if (vm.count("urls"))
+    if (vm.count("inputs"))
     {
-        auto urls = vm["urls"].as<std::vector<std::string>>();
-        for (auto url : urls)
+        auto inputs = vm["inputs"].as<std::vector<std::string>>();
+        for (auto inputstring : inputs)
         {
-            std::cout << url << std::endl;
+            std::string url {};
+            std::string filename {};
+            detectOutputName(inputstring, url, filename);
+            std::cout << url << "\t" << filename << std::endl;
         }
     }
+}
 
+static void detectOutputName(std::string string, std::string& url, std::string& filename, std::string delimiter)
+{
+    std::regex filter { delimiter };
+    std::smatch sm;
+    std::regex_search(string, sm, filter);
+    if (sm.empty() == false)
+    {
+        auto pos { sm.position(0) };
+        url = string.substr(0, pos);
+        filename = string.substr(pos+delimiter.length());
+    }
+    else
+    {
+        url = string;
+        filename = getDefaultFilename(url);
+    }
 }
