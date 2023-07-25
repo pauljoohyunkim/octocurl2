08cc3d8af4aeda8c387f271336f91c076738b739
pauljoohyunkim <pauljoohyunkim@gmail.com>
2023-07-25 16:18:40 +0200

    curl perform

--- src/filename.cpp
+++ src/filename.cpp
@@ -1,12 +1,20 @@
 #include "filename.hpp"
 #include "curlwrap.hpp"
 #include <curl/curl.h>
+#include <iostream>
 #include <memory>
 #include <string>
 
 std::string getDefaultFilename(std::string url)
 {
     auto smart_curl { smart_curl_constructor() };
+    auto curl { (CURL*) smart_curl.get() };
+    CURLcode res;
+    if (curl)
+    {
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        res = curl_easy_perform(curl);
+    }
     
     return std::string("asdf");
 }
